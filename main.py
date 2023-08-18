da1aec5d99ad7a8e03200fb224c7c34cad09d034
tewbo <shotgun1317@gmail.com>
2023-08-18 10:01:07 +0300

    * little change before removing main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from io import BytesIO
+
 from Uploader import Uploader
 from download import download
 
 if __name__ == "__main__":
-    filename = "storage/espresso.jpg"
-    uploader = Uploader(filename)
+    filename = "monster.png"
+    file_io = BytesIO()
+    with open(filename, "rb") as file:
+        file_io.write(file.read())
+    uploader = Uploader(filename, file_io)
     fs_id = uploader.upload()
     print(fs_id)
-    download(fs_id)
+    result = BytesIO()
+    download(fs_id, result)
+    with open("kek.jpg", "wb") as file:
+        result.seek(0)
+        file.write(result.read())
